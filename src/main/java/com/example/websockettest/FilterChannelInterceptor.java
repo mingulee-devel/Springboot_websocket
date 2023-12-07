90ca9c1f8229149457deca2e4703a58b9c23cb6e
mslee <devel.moon17@gmail.com>
2023-12-07 17:31:18 +0900

    웹소켓 헤더 토큰값 전달 테스트 중

---
+++ src/main/java/com/example/websockettest/FilterChannelInterceptor.java
@@ -0,0 +1,43 @@
+package com.example.websockettest;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 99) //Spring Security보다 인터셉터의 우선순위 높이기
+public class FilterChannelInterceptor implements ChannelInterceptor {
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) { //메세지가 채널로 전송되기 전에 호출되는 메소드
+        StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        //StompHeaderAccessor를 이용하여 STOMP 헤더에 접근할 수 있다.
+        //클라이언트에서 커스텀 헤더에 JWT를 실어 보냈으므로 Accessor로 접근
+
+        assert headerAccessor != null;
+
+        if (headerAccessor.getCommand() == StompCommand.CONNECT) { // 연결 시에한 header 확인
+            String token = String.valueOf(headerAccessor.getNativeHeader("Authorization").get(0));
+//            token = token.replace(JwtProperties.TOKEN_PREFIX, "");
+
+            try {
+//                Integer userId = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
+//                        .getClaim("id").asInt();
+
+//                headerAccessor.addNativeHeader("User", String.valueOf(userId));
+                System.out.println("test : " + token);
+            } catch (Exception e){
+
+            }
+//            } catch (TokenExpiredException e) {
+//                e.printStackTrace();
+//            } catch (JWTVerificationException e) {
+//                e.printStackTrace();
+//            }
+        }
+        return message;
+    }
+}
---
+++ src/main/java/com/example/websockettest/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.example.websockettest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat")
+    public void sendMessage(ChatDto chatDto, SimpMessageHeaderAccessor accessor) {
+        simpMessagingTemplate.convertAndSend("/sub/chat/" + chatDto.getChannelId(), chatDto);
+    }
+}
